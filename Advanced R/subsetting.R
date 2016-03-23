15675ab2f6646b1a833850bedec74e98d452dcbd
Erik Istre <e.istre91@gmail.com>
2016-03-23 22:51:17 +1300

    Complete subsetting chapter

--- Advanced R/subsetting.R
+++ Advanced R/subsetting.R
@@ -109,3 +109,62 @@ mtcars[] <- lapply(mtcars, as.integer)
 mtcars <- lapply(mtcars, as.integer)
 data(mtcars)
 
+#Applications
+#Lookup tables
+x <- c("m", "f", "u", "f", "f", "m", "m")
+lookup <- c(m = "Male", f = "Female", u = NA)
+lookup[x]
+unname(lookup[x])
+#Matching and merging by hand
+grades <- c(1, 2, 2, 3, 1)
+info <- data.frame(
+  grade = 3:1,
+  desc = c("Excellent", "Good", "Poor"),
+  fail = c(F, F, T)
+)
+id <- match(grades, info$grade)
+info[id, ]
+rownames(info) <- info$grade
+info[as.character(grades), ]
+#Random samples
+df <- data.frame(x = rep(1:3, each = 2), y = 6:1, z = letters[1:6])
+set.seed(10)
+df[sample(nrow(df)), ]
+df[sample(nrow(df), 3), ]
+df[sample(nrow(df), 6, rep = T), ]
+#Ordering
+x <- c("b", "c", "a")
+x[order(x)]
+#Expanding aggregated counts
+df <- data.frame(x = c(2, 4, 1), y = c(9, 11, 6), n = c(3, 5, 1))
+df[rep(1:nrow(df), df$n), ]
+#Removing columns from data frames
+#Selecting rows based on a condition
+  #Remember to use & and | which are vector boolean operators
+  #These don't short circuit like && and ||, use those in if statements
+mtcars[mtcars$gear == 5 & mtcars$cyl == 4, ]
+subset(mtcars, gear == 5 & cyl == 4)
+#Boolean algebra vs sets
+#which converts booleans to integer indices
+x <- sample(10) < 4
+unwhich <- function(x, n) {
+  out <- rep_len(FALSE, n)
+  out[x] <- TRUE
+  out
+}
+unwhich(which(x), 10)
+
+#Exercises
+#How would you randomly permute the columns of a data frame? 
+#Can you simultaneously permute the rows and columns in one step?
+mtcars[, sample(ncol(mtcars))]
+mtcars[sample(nrow(mtcars)), sample(ncol(mtcars))]
+#How would you select a random sample of m rows from a data farme?
+#What if the sample had to be contiguous?
+mtcars[sample(nrow(mtcars), 5), ]
+first <- sample(nrow(mtcars), 1)
+last <- first + 5
+mtcars[first:last,]
+#How would you put the columns in a data frame in alphabetical order?
+names(mtcars)
+mtcars[, order(names(mtcars))]
